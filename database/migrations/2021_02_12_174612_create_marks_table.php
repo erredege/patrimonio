df3d49074e9bca5368eced6799732013a8d1e703
ivan montoya sanchez <montoyaivan97@gmail.com>
2021-02-12 19:09:49 +0100

    migrations create

---
+++ database/migrations/2021_02_12_174612_create_marks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->double("latitude");
+            $table->double("length");
+            $table->string("name");
+            $table->text("information");
+            $table->enum("type",["marker","circle","polygon"]);
+            $table->double("start_point");
+            $table->double("radio");
+            $table->string("border_color");
+            $table->string("background_color");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+}
---
+++ database/migrations/2021_02_12_180333_create_points_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->double("latitude");
+            $table->double("length");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+}
