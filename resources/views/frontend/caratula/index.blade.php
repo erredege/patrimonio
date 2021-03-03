1ca95c72bd80e7ddbdbda3c5dcb88970bcb36b1f
ivan montoya sanchez <montoyaivan97@gmail.com>
2021-03-03 20:18:35 +0100

    trabajando css caratula

--- resources/views/frontend/caratula/index.blade.php
+++ resources/views/frontend/caratula/index.blade.php
@@ -4,24 +4,17 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="caratula.css">
+        <link rel="stylesheet" href="{{'css/caratula.css'}}">
         <title>Document</title>
     </head>
     <body>
-        <div id="contenedor"  style="background-image:".
-                                @foreach($optionList as $option)
-                                    @if(($option->Key)=="backgroundImage")
-                                        $option->value                                                                                  
-                                    @endif. 
-                                @endforeach
-        >
-            <div>
-                <p><a href="{{route('map.index')}}">Mapa</p>
-            </div>
-            <div>
-                <p><a href="">Juego</p>
-            </div>
+        <div>                       
+            <p><a href="{{route('map.index')}}">Mapa</p>
         </div>
+        <div>
+            <p><a href="">Juego</p>
+        </div>
+       
         <div id="footer">
             
         </div>
