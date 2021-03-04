af9b7b99ab053a36e90ac7588f15500f87fd80e8
erredege <34535136+erredege@users.noreply.github.com>
2021-03-04 19:44:04 +0100

    test

--- resources/views/frontend/map/index.blade.php
+++ resources/views/frontend/map/index.blade.php
@@ -260,25 +260,25 @@
     <div id ="footer-map">
         <div id=galeria >
             <!--aqui va la galertia-->
-              <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    @foreach ($imageList as $image)
-                        @if ($image->title == $marker->title)
-                            <div class="carousel-item active">
-                                <img class="d-block w-100" src="{{$image->route }}" alt="{{$image->title}}">
-                            </div>
-                        @endif 
-                    @endforeach
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        @foreach ($imageList as $image)
+                            @if ($image->title == $marker->title)
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="{{$image->route }}" alt="{{$image->title}}">
+                                </div>
+                            @endif 
+                        @endforeach
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>  
         </div>
         <div id="info" ></div>
     </div>
