cb7f55567915fc5b6c415e6e767ae2aa82ab8a9c
ivan montoya sanchez <montoyaivan97@gmail.com>
2021-03-04 20:49:52 +0100

    caratula ok

--- resources/views/frontend/caratula/index.blade.php
+++ resources/views/frontend/caratula/index.blade.php
@@ -9,20 +9,38 @@
     </head>
     <style>
         body{
-            background-image:url( {{url('img/caratula.jpg')}} );
-
+            background-image: url({{url('img/caratula.jpg')}});
+            background-size: cover;
+            background-repeat: no-repeat;
         }
     </style>
     <body class="animate__bounceInLeft">
 
-        <ul class="menu-bar">
-            <li>
-                <a href="{{route('map.index')}}">MAPA
-            </li>
-            <li>
-                <a href="">JUEGO
-            </li>
-        </ul>
-
+        <div class="all">
+            <div class="left" id="juego" onclick="redirect(this.id)">
+                <div  class="text" >Juego</div>
+            </div>
+            <div class="center" id="mapa" onclick="redirect(this.id)">
+              <div class="explainer"><span>Almería</span></div>
+              <div  class="text" > Mapa </div>
+            </div>
+            <div class="right" id="colaboradores" onclick="redirect(this.id)">
+              <div  class="text" >Colaboradores</div>
+            </div>
+        </div>
     </body>
+    <script>
+    function redirect(id){
+            
+        if(id=="juego"){
+            window.location.href = ''; 
+        }else if(id=="mapa"){
+            window.location.href ='{{route('map.index')}}';  
+        }else{
+            window.location.href = '';
+        }
+    }
+    </script>
 </html>
+
+<!--a href="{{route('map.index')}}"-->
