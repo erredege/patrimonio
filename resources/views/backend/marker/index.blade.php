19fc6cc208a2591c402b3aac72316fe6dbfb3e36
ivan montoya sanchez <montoyaivan97@gmail.com>
2021-03-11 12:21:13 +0100

    trabajando en los botones del escudoCelia | Juego OK

--- resources/views/backend/marker/index.blade.php
+++ resources/views/backend/marker/index.blade.php
@@ -7,7 +7,7 @@
 @section("content")
 <br>
 <a href="{{ route('marker.create') }}">Nuevo</a>
-<table border='1' class="table">
+<table border='1' class="table table-hover">
     @foreach ($markersList as $marker)
 
     @if ($marker->type == 0)
@@ -23,43 +23,47 @@
             $valor = "poligono"
         @endphp
     @endif
+        <thead>
+            <tr>
+                <th>Titulo</th>
+                <th>Nombre</th>
+                <th>Latitud</th>
+                <th>Longitud</th>
+                <th>Informacion</th>
+                <th>Tipo</th>
+                <th>Punto Inicio</th>
+                <th>Radio</th>
+                <th>Color Borde</th>
+                <th>Color Fondo</th>
+                <th>Opacidad</th>
+                <th colspan="2">Opciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{$marker->title}}</td>
+                <td class="marker">{{$marker->name}}</td>
+                <td class="marker">{{$marker->latitude}}</td>
+                <td class="marker">{{$marker->length}}</td>
+                <td class="overflow-auto">{{$marker->information}}</td>
+                <td>{{$valor}}</td>
+                <td>{{$marker->start_point}}</td>
+                <td>{{$marker->radio}}</td>
+                <td>{{$marker->border_color}}</td>
+                <td>{{$marker->background_color}}</td>
+                <td>{{$marker->opacity}}</td>
+                <td><a href="{{route('marker.edit', $marker->id)}}">Modificar</a></td>
+                <td>
+                    <form action = "{{route('marker.destroy', $marker->id)}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <input type="submit" value="Borrar">
+                    </form>
 
-        <tr>
-            <td>Titulo</td>
-            <td>Nombre</td>
-            <td>Latitud</td>
-            <td>Longitud</td>
-            <td>Informacion</td>
-            <td>Tipo</td>
-            <td>Punto Inicio</td>
-            <td>Radio</td>
-            <td>Color Borde</td>
-            <td>Color Fondo</td>
-            <td>Opacidad</td>
-            <td colspan="2">Opciones</td>
-        </tr>
-        <tr>
-            <td>{{$marker->title}}</td>
-            <td class="marker">{{$marker->name}}</td>
-            <td class="marker">{{$marker->latitude}}</td>
-            <td class="marker">{{$marker->length}}</td>
-            <td class="col-xs-3">{{$marker->information}}</td>
-            <td>{{$valor}}</td>
-            <td>{{$marker->start_point}}</td>
-            <td>{{$marker->radio}}</td>
-            <td>{{$marker->border_color}}</td>
-            <td>{{$marker->background_color}}</td>
-            <td>{{$marker->opacity}}</td>
-            <td><a href="{{route('marker.edit', $marker->id)}}">Modificar</a></td>
-            <td>
-                <form action = "{{route('marker.destroy', $marker->id)}}" method="POST">
-                    @csrf
-                    @method("DELETE")
-                    <input type="submit" value="Borrar">
-                </form>
-
-                {{--<button type="button" onclick="destroy('{{route('marker.destroy',$marker->id)}}')">Eliminar</button>--}}
-            </td>
+                    {{--<button type="button" onclick="destroy('{{route('marker.destroy',$marker->id)}}')">Eliminar</button>--}}
+                </td>
+            </tr>
+        </tbody>
     @endforeach
 </table>
 @endsection
