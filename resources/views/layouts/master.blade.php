78ac814fe1d30f59522c3402a99ef1faf07c404f
erredege <34535136+erredege@users.noreply.github.com>
2021-03-16 13:23:49 +0100

    modifico master layouts

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -50,7 +50,6 @@
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                   <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href='index.php'>INICIO <span class="sr-only">(current)</span></a>
                     <a class="nav-item nav-link" href='{{route("marker.index")}}'>MARCADORES<a>
                     <a class="nav-item nav-link" href='{{route("option.index")}}'>OPCIONES<a>
                     <a class="nav-item nav-link" href='{{route("image.index")}}'>IMAGENES<a>
